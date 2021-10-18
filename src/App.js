c8cb0dfe1bfa9675fafb9d78a8cd4c585266e5ac
yousri12 <yousrymahmoud97@gmail.com>
2021-10-18 13:10:09 +0200

    add counter slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Counter from "./component/counter";
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>COUNTER</h2>
+      <div className="flex">
+        <button className="dec" onClick={() => setCounter(counter - 1)}>
+          {" "}
+          dec
+        </button>
+
+        <Counter counter={counter} />
+
+        <button className="inc" onClick={() => setCounter(counter + 1)}>
+          {" "}
+          inc
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/component/counter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./counter.css";
+function Counter({ counter }) {
+  return <div className="counter">{counter}</div>;
+}
+
+export default Counter;
